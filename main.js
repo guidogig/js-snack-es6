bcbdc6ebb0f88bab38acae6e5c4108669ffec405
Guido Giglioli <guido.tobugroup@gmail.com>
2026-06-09 14:21:41 +0200

    snack 1

---
+++ main.js
@@ -0,0 +1,26 @@
+const makeBike = (nome, peso) => ({ nome, peso });
+
+const garage = [
+    makeBike('Trek Émonda SLR 9', 6.6),
+    makeBike('Specialized Tarmac SL7', 2.8),
+    makeBike('Cannondale SuperSix EVO Hi-MOD', 6.9)
+]
+
+const trovaLeggera = (elenco) => {
+    let piuLeggera = elenco[0];
+    console.log('Prima iterazione piu leggera: ', piuLeggera);
+    for (let i = 0; i < elenco.length; i++) {
+        console.log('elenco[i] = ', elenco[i]);
+        if (elenco[i].peso > piuLeggera.peso) {
+            piuLeggera = elenco[i];
+            console.log('piuLeggera = ', piuLeggera);
+        }
+    }
+    return(piuLeggera.nome);
+}
+
+console.log('Garage: ', garage);
+
+console.log('La più leggera del garage é: ',trovaLeggera(garage));
+
+
